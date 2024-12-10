799951dd8f743cd5215e5f995a6ae8715c20b31b
Abderrahmane Maftah <78481502+amaftah@users.noreply.github.com>
2024-12-10 01:43:20 +0000

    t

--- front/my-portfolio/src/data/projectsData.ts
+++ front/my-portfolio/src/data/projectsData.ts
@@ -1,15 +1,21 @@
 export const projectsData = [
   {
-    title: 'Project One',
-    description: 'A brief description of Project One.',
-    imageUrl: '/assets/project1.jpg',
-    link: 'https://github.com/yourusername/project-one'
+    title: 'TE Locker Management',
+    description: 'A brief description of Project OneTE Locker Management is an application for managing lockers at TE Connectivity. ',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/TE_Connectivity_logo.svg',
+    link: 'https://github.com/amaftah/te-finale-project-'
   },
   {
-    title: 'Project Two',
-    description: 'A brief description of Project Two.',
-    imageUrl: '/assets/project2.jpg',
+    title: 'Real eastate agency advertisements app v2',
+    description: 'web application for the management of the advertisements of a real estate agency.',
+    imageUrl: 'https://agenz.ma/_image?href=https%3A%2F%2Fimages.agenz.ma%2Fuploads%2Fprofil-163eeb82-7-agence-immobiliere-agenz.jpeg&w=140&h=140&f=webp',
     link: 'https://github.com/yourusername/project-two'
   },
-  // Add more projects here
+  {
+    title: 'Neumorphism UI/UX Design Portfolio Website in ReactJS',
+    description: 'Neumorphism-inspired UI/UX design portfolio and resume website crafted with ReactJS. ',
+    imageUrl: 'https://cdn.dribbble.com/users/2446071/screenshots/9890709/media/6c9cb8deabc71b3bb158f6ab7ca653ff.png',
+    link: 'https://github.com/amaftah/Portfolio-ReactJs'
+  },
+ // {Add more projects here
 ];
